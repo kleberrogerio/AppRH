364da1d3a6713c311daa36a925e63f3498e95daa
kleberroger <kleber.nascimento5@fatec.sp.gov.br>
2023-04-27 22:20:57 -0300

    Começando a inserir Funcionário e Dependentes

--- src/main/java/com/AppRH/AppRH/models/Dependentes.java
+++ src/main/java/com/AppRH/AppRH/models/Dependentes.java
@@ -1,5 +1,85 @@
 package com.AppRH.AppRH.models;
 
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="func_dep")
 public class Dependentes {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="func_dep_id")
+	private int funcdepid;
+	
+	@Column(name="func_dep_cpf",unique = true)
+	private String funcdepcpf;
+	
+	@Column(name="func_dep_nome")
+	private String funcdepnome;
+	
+	
+	@Column(name="func_dep_data")
+	private Date funcdepdata;
+	
+	
+	public int getFuncdepid() {
+		return funcdepid;
+	}
+
+	public void setFuncdepid(int funcdepid) {
+		this.funcdepid = funcdepid;
+	}
+
+	public String getFuncdepcpf() {
+		return funcdepcpf;
+	}
+
+	public void setFuncdepcpf(String funcdepcpf) {
+		this.funcdepcpf = funcdepcpf;
+	}
+
+	public String getFuncdepnome() {
+		return funcdepnome;
+	}
+
+	public void setFuncdepnome(String funcdepnome) {
+		this.funcdepnome = funcdepnome;
+	}
+	
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+	
+
+	public Date getFuncdepdata() {
+		return funcdepdata;
+	}
+
+	public void setFuncdepdata(Date funcdepdata) {
+		this.funcdepdata = funcdepdata;
+	}
+
+
+	
+	@ManyToOne
+	@JoinColumn(name = "func_matricula")
+	private Funcionario funcionario;
+
+		
+	
 }
--- src/main/java/com/AppRH/AppRH/models/Funcionario.java
+++ src/main/java/com/AppRH/AppRH/models/Funcionario.java
@@ -1,69 +1,94 @@
 package com.AppRH.AppRH.models;
 
 import java.io.Serializable;
+import java.sql.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-public class Funcionario {
+
+@Entity
+@Table(name="func")
+public class Funcionario implements Serializable{
 	
 private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="func_id")
+	private int funcid;
 	
-	private String nome;
-	private String data;
-	private String email;
+	@Column(name="func_matricula")
+	private int funcmatricula;
+	
+	@Column(name="func_nome")
+	private String funcnome;
+	
+	@Column(name="func_data")
+	private Date funcdata;
+	
+	@Column(name="func_email")
+	private String funcemail;
 	
 	@OneToMany(mappedBy = "funcionario", cascade = CascadeType.REMOVE)
 	private List<Dependentes>dependentes;
 
-	public long getId() {
-		return id;
+		public int getFuncid() {
+		return funcid;
 	}
 
-	public void setId(long id) {
-		this.id = id;
+	public void setFuncid(int funcid) {
+		this.funcid = funcid;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getFuncnome() {
+		return funcnome;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setFuncnome(String funcnome) {
+		this.funcnome = funcnome;
 	}
 
-	public String getData() {
-		return data;
+	public Date getFuncdata() {
+		return funcdata;
 	}
 
-	public void setData(String data) {
-		this.data = data;
+	public void setFuncdata(Date funcdata) {
+		this.funcdata = funcdata;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getFuncemail() {
+		return funcemail;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setFuncemail(String funcemail) {
+		this.funcemail = funcemail;
+	}
+	
+	
+
+	public int getFuncmatricula() {
+		return funcmatricula;
 	}
 
-	public List<Dependentes> getDependentes() {
-		return dependentes;
+	public void setFuncmatricula(int funcmatricula) {
+		this.funcmatricula = funcmatricula;
 	}
 
 	public void setDependentes(List<Dependentes> dependentes) {
 		this.dependentes = dependentes;
 	}
-	
-	
+
+		public List<Dependentes> getDependentes() {
+		return dependentes;
+	}
+
+		
 }
