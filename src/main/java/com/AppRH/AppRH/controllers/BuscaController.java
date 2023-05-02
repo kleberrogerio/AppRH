34091b6bc87c26755e7f6842a1410b4ea1fc340f
kleberroger <kleber.nascimento5@fatec.sp.gov.br>
2023-05-01 23:28:26 -0300

    Arrumando o Index

--- src/main/java/com/AppRH/AppRH/controllers/BuscaController.java
+++ src/main/java/com/AppRH/AppRH/controllers/BuscaController.java
@@ -26,5 +26,34 @@ public class BuscaController {
 	@Autowired
 	private TelefoneRepository tr;
 	
+	//Get
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView abrirIndex() {
+		ModelAndView mv = new ModelAndView("index");
+		return mv;
+	}
 	
+	//Post
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public ModelAndView buscarIndex(@RequestParam("buscar") String buscar, @RequestParam("coopnome") String coopnome) {
+		
+		ModelAndView mv = new ModelAndView("index");
+		String mensagem = "Resultados da busca por " + buscar;
+		
+		if(coopnome.equals("nomecooperado")){
+			mv.addObject("cooperados",cr.findByCoopnomesCooperado(buscar));
+		}else if(coopnome.equals("nomedependente")) {
+			mv.addObject("cooperados",cr.findByCoopnomesCooperado(buscar));
+		}else if(coopnome.equals("nomefuncionario")) {
+			mv.addObject("cooperados",cr.findByCoopnomesCooperado(buscar));
+		}else if(coopnome.equals("nometelefone")) {
+			mv.addObject("cooperados",cr.findByCoopnomesCooperado(buscar));
+		}else {
+			mv.addObject("cooperados",cr.findByCoopnomesCooperado(buscar));
+		}
+		
+		mv.addObject("mensagem", mensagem);
+		
+		return mv;	
+	}
 }
