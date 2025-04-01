c572690c5a9eb500913cc54ed757f5f136bc9484
kleberrogerio <kleber.nascimento5@fatec.sp.gov.br>
2025-04-01 13:12:33 -0300

    Inserindo foto de Cooperado

--- src/main/java/com/AppRH/AppRH/controllers/InteressadoController.java
+++ src/main/java/com/AppRH/AppRH/controllers/InteressadoController.java
@@ -1,23 +1,35 @@
 package com.AppRH.AppRH.controllers;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.AppRH.AppRH.models.Funcionario;
 import com.AppRH.AppRH.models.Interessado;
+import com.AppRH.AppRH.repository.CotaRepository;
+import com.AppRH.AppRH.repository.InteressadoRepository;
 
 public class InteressadoController {
 	
+	@Autowired
+	private InteressadoRepository ir;
+	
 	//LISTAR COOPERADOS
-		@GetMapping("/interessados")
 		@PreAuthorize("hasAnyRole('ADMIN', 'USUARIO','DEVELOPER')")
+		@GetMapping("/interessados")		
 		public ModelAndView listaInteressados() {
 			ModelAndView mv = new ModelAndView("interessado/listaInteressados");
-			Iterable<Interessado>interessados= new ArrayList<Interessado>();	
-			mv.addObject("interessados",interessados);
+			Iterable<Interessado> interessadosIterable = ir.findAll(); // Recupera os funcionários como Iterable
+			List<Interessado> interessados = new ArrayList<>(); // Converte Iterable para List
+			interessadosIterable.forEach(interessados::add); // Adiciona os funcionários à lista
+		 	mv.addObject("interessados",interessados);
 			return mv;
-		}
+		}    
+		 
 
 }
--- src/main/java/com/AppRH/AppRH/repository/InteressadoRepository.java
+++ src/main/java/com/AppRH/AppRH/repository/InteressadoRepository.java
@@ -3,11 +3,17 @@ package com.AppRH.AppRH.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.AppRH.AppRH.models.Funcionario;
 import com.AppRH.AppRH.models.Interessado;
 
-public interface InteressadoRepository extends JpaRepository<Interessado, Long>{
+public interface InteressadoRepository extends JpaRepository<Interessado, String>{
 	
 	List<Interessado> findByFuncpronome(String nome);
+	//Para Buscar
+		@Query(value = "SELECT * FROM profissionais p", nativeQuery = true)
+		List<Interessado> findAll();
+		
 
 }
